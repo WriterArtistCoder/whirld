1c8042c4d857a7c4ae45353a2b22bbee5c29e66a
WriterArtistCoder <tinystripz@outlook.com>
2025-01-28 15:23:35 -0800

    Logging improvements May be too many bells and whistles, test performance with/without

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,21 @@ import { serve } from "@hono/node-server"
 import { Hono } from "hono"
 import { cors } from "hono/cors"
 import { GoogleTranslate } from "./GoogleCloud/GoogleTranslate"
+import type { UnofficialStatusCode } from "hono/utils/http-status"
 
 const app = new Hono()
 const port = parseInt(process.env.PORT || "3193")
 const groot = new GoogleTranslate()
 
 const LANGUAGES = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'zh', 'zh-TW', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'he', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jv', 'kn', 'kk', 'km', 'rw', 'ko', 'ku', 'ky', 'lo', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ny', 'or', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tl', 'tg', 'ta', 'tt', 'te', 'th', 'tr', 'tk', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'];
+enum Pref {
+    ALWAYS,
+    DEFAULT,
+    NEVER
+}
 const WHAT_TO_LOG = {
-    languages: true,
-    progress: true
+    languages: Pref.DEFAULT,
+    progress: Pref.ALWAYS
 }
 const OH_CRAP_LIMIT = 5
 
@@ -20,7 +26,7 @@ app.use("/*", cors())
 // Scrmable a text
 app.post("/api/scramble", async (c) => {
     c.status(200)
-    console.log(`\x1b[1m  \n\n\n/api/scramble \x1b[0m`)
+    console.log(`\x1b[1m  \n\n\nPOST /api/scramble \x1b[0m`)
     
     // TODO add maximum request time, abort with 504 if exceeded
 
@@ -31,6 +37,8 @@ app.post("/api/scramble", async (c) => {
     let totalProblems = 0, problemsInARow = 0
     let langLog = lang
     let translation = text
+
+    let BEGINNING = Date.now()
 
     if (!Number.isInteger(times) || times<2) {
         c.status(400)
@@ -54,11 +62,11 @@ app.post("/api/scramble", async (c) => {
                 translation = await groot.translateText(translation, nextLang, prevLang)
             } catch (e) {
                 totalProblems++
-                c.status(222) // Ad hoc HTTP code meaning 'error was overcome'
+                c.status(222 as UnofficialStatusCode) // Ad hoc HTTP code meaning 'error was overcome'
 
                 // Log that there was an error
                 // TODO log specifics
-                console.log(`\x1b[30;103m${new Date().toISOString()}\x1b[0m \x1b[30;101m Unexpected error \x1b[0m \n`)
+                console.log(`\x1b[30;103m${dateStr(BEGINNING)}\x1b[0m \x1b[30;101m Unexpected error \x1b[0m \n`)
 
                 if (++problemsInARow < OH_CRAP_LIMIT) {
                     // Reset and try again
@@ -67,7 +75,9 @@ app.post("/api/scramble", async (c) => {
                     // Give up if there were too many problems in a row
 
                     try {
-                        c.status(299) // Ad hoc HTTP code meaning 'error was partially overcome'
+                        console.log(`\x1b[103m${dateStr(BEGINNING)}\x1b[0m \x1b[101m Too many errors in a row, cutting our losses \x1b[0m \n`)
+                        
+                        c.status(299 as UnofficialStatusCode) // Ad hoc HTTP code meaning 'error was partially overcome'
                         return c.json({
                             bamboozled: translation,
                             original: text,
@@ -89,27 +99,42 @@ app.post("/api/scramble", async (c) => {
 
         // Logging
 
-        let DATE = `\x1b[93m${new Date().toISOString()}`
-
-        // TODO add logging of elapsed time
+        let DATE = `\x1b[93m${dateStr(BEGINNING)}` // TODO add logging of elapsed time
 
         let LANGLOG = ''
 
-        if (WHAT_TO_LOG.languages) {
-            // Languages used so far, up to most recent translation (highlight in green)
-            const ll1 = langLog.substring(0, langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1))
-            
-            // Most recent translation (e.g. en > fr), highlighted in white
-            const ll2 = langLog.substring(langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1))
-            
-            LANGLOG = `\x1b[92m${ll1}\x1b[0m${ll2}`
+        switch (WHAT_TO_LOG.languages) {
+            case Pref.DEFAULT:
+                if (langLog.length > 80-25) {
+                    // Original language (highlight in green)
+                    const ll1 = langLog.substring(0, langLog.indexOf(' > ')) + ' > ...'
+
+                    // Most recent translation (e.g. en > fr), highlighted in white
+                    const ll2 = langLog.substring(langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1))
+
+                    LANGLOG = `\x1b[92m${ll1}\x1b[0m${ll2}`
+                    break
+                }
+
+            case Pref.ALWAYS:
+                // Languages used so far, up to most recent translation (highlight in green)
+                const ll1 = langLog.substring(0, langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1)+1)
+                
+                // Most recent translation (e.g. en > fr), highlighted in white
+                const ll2 = langLog.substring(langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1)+1)
+                
+                LANGLOG = `\x1b[92m${ll1}\x1b[0m${ll2}`
+                break
         }
 
         let PROGRESS = ''
-        if (WHAT_TO_LOG.progress) {
-            PROGRESS = `\x1b[91m \n${i}/${times} (${Math.floor(1000*i/times)/10}%)`;
-            if (totalProblems>0) PROGRESS += ` +${totalProblems} aborted attempt`
-            if (totalProblems>1) PROGRESS += 's'
+        switch (WHAT_TO_LOG.progress) {
+            case Pref.DEFAULT:
+            case Pref.ALWAYS:
+                PROGRESS = `\x1b[91m \n${i}/${times} (${(100*i/times).toFixed(1)}%)`;
+                if (totalProblems>0) PROGRESS += ` +${totalProblems} aborted attempt`
+                if (totalProblems>1) PROGRESS += 's'
+                break
         }
 
         let TRANSLATION = `\n \x1b[34m${translation}\n`
@@ -129,6 +154,24 @@ app.post("/api/scramble", async (c) => {
 })
 
 console.log(`WHIRLD server is running on port ${port}`)
+
+// TODO overly complicated?
+function dateStr(BEGINNING: number) {
+    let string = ''
+    string += new Date().toISOString()
+
+    let mss = ''
+    let ms = Date.now()-BEGINNING
+
+    if (ms >= 10000) mss = (ms/1000).toFixed(2) + 's'
+    else if (ms >= 100) mss = (ms/1000).toFixed(3) + 's'
+    else mss = ms + 'ms'
+
+    string += ' ' + '       '.substring(mss.length)
+    string += mss
+
+    return string
+}
 
 serve({
     fetch: app.fetch,
