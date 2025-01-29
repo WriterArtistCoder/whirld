9fe97e2fec1b9b3f07d439206b7252a0b5250192
WriterArtistCoder <tinystripz@outlook.com>
2025-01-28 16:31:44 -0800

    Input validation, benched dateStr(), WIP README

--- server/README.md
+++ server/README.md
@@ -1,15 +1,21 @@
 # whirld-server
 
-To install dependencies:
-
 ```bash
-bun install
-```
-
-To run:
-
-```bash
-bun run index.ts
+bun install  # Install dependencies
+yarn dev     # Run server
 ```
 
 This project was created using `bun init` in bun v1.2.1. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+# POST /api/scramble
+## Client:
+```json
+{
+    "lang": "en",  ISO-639 language code
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis vulputate leo, ac porttitor ex. Phasellus dignissim diam ac nulla aliquet dictum. Donec scelerisque, lectus sit amet pellentesque porta, nunc leo blandit felis, ut imperdiet nunc risus vitae nulla. Pellentesque feugiat vitae nisi ac pretium. Ut in dapibus arcu, ac hendrerit metus. Nulla tempus neque ultrices nulla laoreet facilisis. Donec id mi mattis, tincidunt diam non, molestie nisl. Vivamus bibendum libero ex, a tempor sapien vulputate laoreet. Nullam nisi mauris, ullamcorper et eleifend et, placerat sit amet libero.",
+    "times": 100   Number of translations
+}
+```
+
+## Server:
+I Have Not Written This Part Yet
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 import { serve } from "@hono/node-server"
 import { Hono } from "hono"
 import { cors } from "hono/cors"
+import { timeout } from 'hono/timeout'
 import { GoogleTranslate } from "./GoogleCloud/GoogleTranslate"
 import type { UnofficialStatusCode } from "hono/utils/http-status"
+import { HTTPException } from "hono/http-exception"
 
 const app = new Hono()
 const port = parseInt(process.env.PORT || "3193")
 const groot = new GoogleTranslate()
 
+const DEFAULT_LANGUAGE = 'en' // Final language is English if none specified
 const LANGUAGES = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'zh', 'zh-TW', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'he', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jv', 'kn', 'kk', 'km', 'rw', 'ko', 'ku', 'ky', 'lo', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ny', 'or', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tl', 'tg', 'ta', 'tt', 'te', 'th', 'tr', 'tk', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'];
 enum Pref {
     ALWAYS,
@@ -19,144 +22,171 @@ const WHAT_TO_LOG = {
     progress: Pref.ALWAYS
 }
 const OH_CRAP_LIMIT = 5
+const TIMEOUT_AFTER = 1000 * 120 // Time out after 2 minutes
 
 // Enable CORS
 app.use("/*", cors())
 
-// Scrmable a text
+// Scramble a text
+
+// TODO Enable timeout, make sure it doesn't keep running after returning HTTP 408
+// app.use("/api/scramble", timeout(TIMEOUT_AFTER, new HTTPException(408)))
+
 app.post("/api/scramble", async (c) => {
     c.status(200)
     console.log(`\x1b[1m  \n\n\nPOST /api/scramble \x1b[0m`)
-    
-    // TODO add maximum request time, abort with 504 if exceeded
 
-    // TODO handle simeltaneous requests (assign ID?)
+    // TODO handle simultaneous requests (assign ID?)
 
-    // TODO Sanitize body and handle 400 formatting errors
-    const { lang, text, times } = await c.req.json()
-    let totalProblems = 0, problemsInARow = 0
-    let langLog = lang
-    let translation = text
+    // TODO Make API more standard in its error handling
 
-    let BEGINNING = Date.now()
+    try {
+        // TODO Sanitize body
+        const { lang, text, times } = await c.req.json()
 
-    if (!Number.isInteger(times) || times<2) {
-        c.status(400)
-        return c.json({
-            forshame: '"times" property should be 2 or more'
-        })
-    } else if (times>500) {
-        c.status(400)
-        return c.json({
-            forshame: '"times" property should be less than 501'
-        })
-    }
+        // Substitute lang for null if invalid
+        const LANG_INVALID = !(typeof lang === 'string') || !LANGUAGES.includes(lang)
 
-    let prevLang = lang
-    for (var i = 0; i < times+1; i++) {
-        if (i>0) {
-            let nextLang = i==times ? lang : LANGUAGES[Math.floor(Math.random() * (LANGUAGES.length))]
-            langLog += ' > ' + nextLang
-            
-            try {
-                translation = await groot.translateText(translation, nextLang, prevLang)
-            } catch (e) {
-                totalProblems++
-                c.status(222 as UnofficialStatusCode) // Ad hoc HTTP code meaning 'error was overcome'
+        // HTTP 400 if text invalid
+        if (!(typeof text === 'string')) {
+            c.status(400)
+            return c.json({
+                forshame: '"text" property should be a string'
+            })
+        }
 
-                // Log that there was an error
-                // TODO log specifics
-                console.log(`\x1b[30;103m${dateStr(BEGINNING)}\x1b[0m \x1b[30;101m Unexpected error \x1b[0m \n`)
+        // HTTP 400 if times invalid
+        if (!Number.isInteger(times) || times<2) {
+            c.status(400)
+            return c.json({
+                forshame: '"times" property should be a number, 2 or more'
+            })
+        } else if (times>500) {
+            c.status(400)
+            return c.json({
+                forshame: '"times" property should be a number, 500 or less'
+            })
+        }
 
-                if (++problemsInARow < OH_CRAP_LIMIT) {
-                    // Reset and try again
-                    nextLang = prevLang; i--; continue
-                } else {
-                    // Give up if there were too many problems in a row
+        let totalProblems = 0, problemsInARow = 0
+        let langLog = LANG_INVALID?'??':lang
+        let translation = text
 
-                    try {
-                        console.log(`\x1b[103m${dateStr(BEGINNING)}\x1b[0m \x1b[101m Too many errors in a row, cutting our losses \x1b[0m \n`)
-                        
-                        c.status(299 as UnofficialStatusCode) // Ad hoc HTTP code meaning 'error was partially overcome'
-                        return c.json({
-                            bamboozled: translation,
-                            original: text,
-                            langs: langLog.split(' > '),
-                            sorry: `${i} of requested ${times} translations were completed`
-                        })
-                    } catch {
-                        c.status(500)
-                        return c.json({
-                            sorry: "Sorry"
-                        })
+        let prevLang = LANG_INVALID?undefined:lang
+        for (var i = 0; i < times+1; i++) {
+            if (i>0) {
+                let nextLang = i==times ? LANG_INVALID?DEFAULT_LANGUAGE:lang : LANGUAGES[Math.floor(Math.random() * (LANGUAGES.length))]
+                langLog += ' > ' + nextLang
+                
+                try {
+                    translation = await groot.translateText(translation, nextLang, prevLang)
+                } catch (e) {
+                    totalProblems++
+                    c.status(222 as UnofficialStatusCode) // Ad hoc HTTP code meaning 'error was overcome'
+
+                    // Log that there was an error
+                    // TODO log specifics
+                    console.log(`\x1b[30;103m${new Date().toISOString()}\x1b[0m \x1b[30;101m Unexpected error \x1b[0m \n`)
+
+                    if (++problemsInARow < OH_CRAP_LIMIT) {
+                        // Reset and try again
+                        nextLang = prevLang; i--; continue
+                    } else {
+                        // Give up if there were too many problems in a row
+
+                        try {
+                            console.log(`\x1b[1;103m${new Date().toISOString()}\x1b[0m \x1b[1;101m Too many errors in a row, cutting our losses \x1b[0m \n`)
+                            
+                            c.status(299 as UnofficialStatusCode) // Ad hoc HTTP code meaning 'error was partially overcome'
+                            return c.json({
+                                bamboozled: translation,
+                                original: text,
+                                langs: langLog.split(' > '),
+                                sorry: `${i} of requested ${times} translations were completed`
+                            })
+                        } catch {
+                            c.status(500)
+                            return c.json({
+                                sorry: "Sorry"
+                            })
+                        }
                     }
                 }
+                prevLang = nextLang
             }
-            prevLang = nextLang
-        }
 
-        problemsInARow = 0
+            problemsInARow = 0
 
-        // Logging
+            // Logging
 
-        let DATE = `\x1b[93m${dateStr(BEGINNING)}` // TODO add logging of elapsed time
+            let DATE = `\x1b[93m${new Date().toISOString()}` // TODO add logging of elapsed time
 
-        let LANGLOG = ''
+            let LANGLOG = ''
 
-        switch (WHAT_TO_LOG.languages) {
-            case Pref.DEFAULT:
-                if (langLog.length > 80-25) {
-                    // Original language (highlight in green)
-                    const ll1 = langLog.substring(0, langLog.indexOf(' > ')) + ' > ...'
+            switch (WHAT_TO_LOG.languages) {
+                case Pref.DEFAULT:
+                    if (langLog.length > 80-25) {
+                        // Original language (highlight in green)
+                        const ll1 = langLog.substring(0, langLog.indexOf(' > ')) + ' > ...'
 
+                        // Most recent translation (e.g. en > fr), highlighted in white
+                        const ll2 = langLog.substring(langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1))
+
+                        LANGLOG = `\x1b[92m${ll1}\x1b[0m${ll2}`
+                        break
+                    }
+
+                case Pref.ALWAYS:
+                    // Languages used so far, up to most recent translation (highlight in green)
+                    const ll1 = langLog.substring(0, langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1)+1)
+                    
                     // Most recent translation (e.g. en > fr), highlighted in white
-                    const ll2 = langLog.substring(langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1))
-
+                    const ll2 = langLog.substring(langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1)+1)
+                    
                     LANGLOG = `\x1b[92m${ll1}\x1b[0m${ll2}`
                     break
-                }
+            }
 
-            case Pref.ALWAYS:
-                // Languages used so far, up to most recent translation (highlight in green)
-                const ll1 = langLog.substring(0, langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1)+1)
-                
-                // Most recent translation (e.g. en > fr), highlighted in white
-                const ll2 = langLog.substring(langLog.lastIndexOf(' > ', langLog.lastIndexOf(' > ')-1)+1)
-                
-                LANGLOG = `\x1b[92m${ll1}\x1b[0m${ll2}`
-                break
+            let PROGRESS = ''
+            switch (WHAT_TO_LOG.progress) {
+                case Pref.DEFAULT:
+                case Pref.ALWAYS:
+                    PROGRESS = `\x1b[91m \n${i}/${times} (${(100*i/times).toFixed(1)}%)`;
+                    if (totalProblems>0) PROGRESS += ` +${totalProblems} aborted attempt`
+                    if (totalProblems>1) PROGRESS += 's'
+                    break
+            }
+
+            let TRANSLATION = `\n \x1b[34m${translation}\n`
+
+            // Date (yellow)
+            //        Languages (green and white)
+            //                              Progress (red)
+            //                                          Translation (blue)
+            console.log(`${DATE} ${LANGLOG} ${PROGRESS} ${TRANSLATION} \x1b[0m`)
         }
 
-        let PROGRESS = ''
-        switch (WHAT_TO_LOG.progress) {
-            case Pref.DEFAULT:
-            case Pref.ALWAYS:
-                PROGRESS = `\x1b[91m \n${i}/${times} (${(100*i/times).toFixed(1)}%)`;
-                if (totalProblems>0) PROGRESS += ` +${totalProblems} aborted attempt`
-                if (totalProblems>1) PROGRESS += 's'
-                break
+        return c.json({
+            bamboozled: translation,
+            original: text,
+            langs: langLog.split(' > ')
+        })
+    } catch (error : any) {
+        if (error instanceof SyntaxError) {
+            console.log(error)
+            c.status(400)
+            return c.json({
+                "forshame": "Invalid JSON"
+            })
         }
-
-        let TRANSLATION = `\n \x1b[34m${translation}\n`
-
-        // Date (yellow)
-        //        Languages (green and white)
-        //                              Progress (red)
-        //                                          Translation (blue)
-        console.log(`${DATE} ${LANGLOG} ${PROGRESS} ${TRANSLATION} \x1b[0m`)
+        else throw error
     }
-
-    return c.json({
-        bamboozled: translation,
-        original: text,
-        langs: langLog.split(' > ')
-    })
 })
 
 console.log(`WHIRLD server is running on port ${port}`)
 
-// TODO overly complicated?
-function dateStr(BEGINNING: number) {
+// TODO Use this (overly complicated?)
+/*function dateStr(BEGINNING: number) {
     let string = ''
     string += new Date().toISOString()
 
@@ -171,7 +201,7 @@ function dateStr(BEGINNING: number) {
     string += mss
 
     return string
-}
+}*/
 
 serve({
     fetch: app.fetch,
