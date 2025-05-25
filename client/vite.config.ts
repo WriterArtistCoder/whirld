352fdb4512f4b8de95d886910a5c9f8522892f1c
WriterArtistCoder <tinystripz@outlook.com>
2025-05-25 12:34:28 -0700

    Secure websocket (wss), now builds w/ relative paths

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
+  base: '', // Relative paths
+  build: {  // Compile to docs
     outDir: '../docs'
   },
   plugins: [react()],
